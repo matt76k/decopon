0288cfa753253a6b4ed909c9f3f368ecec0c0abf
matt <matt76k@gmail.com>
2023-11-07 22:51:46 +0900

    first commit

---
+++ decopon/controller.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import pygame
+
+
+class Controller(ABC):
+    @abstractmethod
+    def update(self) -> Tuple[bool, bool, bool]:
+        return True, True, True
+
+
+class Human(Controller):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def update(self) -> Tuple[bool, bool, bool]:
+        pressedKeys = pygame.key.get_pressed()
+        return pressedKeys[pygame.K_LEFT], pressedKeys[pygame.K_RIGHT], pressedKeys[pygame.K_SPACE]
